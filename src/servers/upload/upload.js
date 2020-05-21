a26505c4925b1b89e6968c7f990e1f5e0ed24a3c
phong  nguyen <phongit1995@gmail.com>
2020-05-21 15:21:36 +0700

    fix upload

--- src/servers/upload/upload.js
+++ src/servers/upload/upload.js
@@ -1,7 +1,11 @@
-import express from 'express';
+
+require("dotenv").config();
+import express, { request } from 'express';
 import multer from 'multer';
 import {uploadCloudinary} from './../../commons/cloudinary';
 import {responsHelper} from './../../commons/responsiveHelper';
+import path from 'path';
+import fs from 'fs';
 const router = express.Router();
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -21,10 +25,22 @@ router.post("/",
     async (req,res)=>{
         try {
             let dataUpload = await uploadCloudinary(req.file.path);
+            if(process.env.DEV=="production"){
+                dataUpload.url= process.env.WEBSITE + req.file.filename ;
+            }
             return responsHelper(req,res,null,{...dataUpload,filename:req.file.filename});
         } catch (error) {
             return responsHelper(req,res,error);
         }
         
     })
+router.get("/:filename",(req,res)=>{
+    if(fs.existsSync(path.join(__dirname,'/../../../uploads',req.params.filename))){
+        let stream = fs.createReadStream(path.join(__dirname,'/../../../uploads',req.params.filename))
+        stream.pipe(res);
+    }
+    else{
+        res.send("Not found")
+    }
+})
 export default router ;
